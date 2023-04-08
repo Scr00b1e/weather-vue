aeda7211e04043cbb150851e204eec26a83f47e9
Jack <kbolot312@gmail.com>
2023-04-08 16:01:50 +0600

    clear

--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
-
 const app = createApp(App)
 
-app.use(router)
-
-app.mount('#app')
+app.use(router).mount('#app')
