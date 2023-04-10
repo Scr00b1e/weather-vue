2e7d9d018bbfe7ae1557fc755fb2ac3e8ea1b1a9
Jack <kbolot312@gmail.com>
2023-04-10 21:57:56 +0600

    fetching data

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,58 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
+import Item from './components/Item.vue';
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      city: ''
+    }
+  },
+  components: {
+    Item
+  },
+  methods: {
+    async getData() {
+      try {
+        const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`)
+        this.city = res.data
+      } catch {
+        alert('Something is wrong...')
+      }
+    },
+    onData() {
+      console.log(this.city)
+    }
+  },
+  // mounted() {
+  //   this.getData()
+  // }
+}
 </script>
 
 <template>
-  <h1>hey</h1>
+  <div class="app">
+    <div>
+      <Item />
+      <button @click="onData">Click me</button>
+    </div>
+  </div>
 </template>
 
-<style></style>
+<style>
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background: #bebcbc;
+}
+
+.app {
+  width: 100%;
+  max-width: max-content;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Item.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        sup bros
+        324 celcium
+        rainy
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
